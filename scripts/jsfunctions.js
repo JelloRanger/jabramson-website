7e97e6d35797c585d4149abbf5c7d63b62e615b0
Jacob Abramson <jacob@jabramson.com>
2015-07-01 21:48:00 -0400

    Added content to projects page

--- scripts/jsfunctions.js
+++ scripts/jsfunctions.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
 
+	// switch content based on menu button clicked, hide others
 	$('#homeBtn').click(function() {
-	  $('#home').toggle('slow').siblings().hide();
+	  $('#home').toggle().siblings().hide();
 	  $(this).toggleClass('btnClicked').siblings().removeClass('btnClicked');
 	});
 
 	$('#aboutBtn').click(function() {
-	  $('#about').toggle('slow').siblings().hide();
+	  $('#about').toggle().siblings().hide();
 	  $(this).toggleClass('btnClicked').siblings().removeClass('btnClicked');
 	});
 
 	$('#projectsBtn').click(function() {
-	  $('#projects').toggle('slow').siblings().hide();
+	  $('#projects').toggle().siblings().hide();
 	  $(this).toggleClass('btnClicked').siblings().removeClass('btnClicked');
 	});
 
 	$('#contactBtn').click(function() {
-	  $('#contact').toggle('slow').siblings().hide();
+	  $('#contact').toggle().siblings().hide();
 	  $(this).toggleClass('btnClicked').siblings().removeClass('btnClicked');
 	});
 
